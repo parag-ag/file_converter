083d4b03a0a2de179d4a3276e334a3b0efe450cf
parag-ag <paraga2719@gmail.com>
2019-06-28 00:23:31 +0530

    Conversion scripts

---
+++ conversion_scripts/json-xml.py
@@ -0,0 +1,6 @@
+from json2xml import json2xml, readfromurl
+
+#converting json to xml
+data = readfromurl("https://coderwall.com/vinitcool76.json")
+data = json2xml.Json2xml(data).to_xml()
+print(data)
