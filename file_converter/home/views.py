b7569064e1fb5d146c3669ffc23998a7e50d6229
vinayak <itssvinayak@gmail.com>
2019-06-28 08:25:39 +0530

    try to convert file : fail

--- file_converter/home/views.py
+++ file_converter/home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render,redirect
 from .forms import DocumentForm
+from json2xml import json2xml
 
 def index(request):
      if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
+
         if form.is_valid():
+            if request.POST.get('file_format') == 'json' and request.POST.get('format_output') == 'xml' :
+                print("converting")
+                request.FILES['document'].read()=jsontoxml(request.FILES['document'].read())
+                print(request.FILES['document'].read())
             form.save()
             return redirect('index')
      else:
@@ -14,3 +20,7 @@ def index(request):
      'form': form
      }
      return render(request,"home/index.html",page_details)
+
+def jsontoxml(data):
+     data=json2xml.Json2xml(data).to_xml()
+     return data
