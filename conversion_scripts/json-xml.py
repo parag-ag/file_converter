559d516c014ef49b7cb7f4454753818ccaa14de4
vinayak <itssvinayak@gmail.com>
2019-06-29 09:08:42 +0530

    now changing

--- conversion_scripts/json-xml.py
+++ conversion_scripts/json-xml.py
@@ -1,7 +1,9 @@
-from json2xml import json2xml, readfromurl
+from json2xml import json2xml,readfromjson
 
-#converting json to xml
-data = readfromurl("https://coderwall.com/vinitcool76.json")
-data = json2xml.Json2xml(data).to_xml()
-print(data)
+data = readfromjson("/home/spider/Downloads/example_2.json")
 
+data=json2xml.Json2xml(data).to_xml()
+
+with open('output.txt', 'w+') as f:
+    f.write(data)
+os.remove("url")
--- file_converter/home/views.py
+++ file_converter/home/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render,redirect
 from .forms import DocumentForm
-from json2xml import json2xml
+from json2xml import json2xml,readfromjson
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 
 def index(request):
      if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
 
         if form.is_valid():
-            if request.POST.get('file_format') == 'json' and request.POST.get('format_output') == 'xml' :
-                print("converting")
-                request.FILES['document'].read()=jsontoxml(request.FILES['document'].read())
-                print(request.FILES['document'].read())
             form.save()
-            return redirect('index')
+            if request.POST.get('file_format') == 'json' and request.POST.get('format_output') == 'xml' :
+                print(jsontoxml(request.FILES['document'].name))
+
+
+        return redirect('index')
      else:
          form = DocumentForm()
      page_details={
@@ -21,6 +26,12 @@ def index(request):
      }
      return render(request,"home/index.html",page_details)
 
-def jsontoxml(data):
-     data=json2xml.Json2xml(data).to_xml()
-     return data
+def jsontoxml(filename):
+    data = readfromjson(MEDIA_ROOT+'/documents/'+filename)
+    data_xml=json2xml.Json2xml(data).to_xml()
+    url=MEDIA_ROOT+'/documents/'+os.path.splitext(filename)[0]+'.xml'
+    rurl=os.path.splitext(filename)[0]+'.xml'
+    with open(url, 'w+') as f:
+        f.write(data_xml)
+    os.remove(MEDIA_ROOT+'/documents/'+filename)
+    return rurl
