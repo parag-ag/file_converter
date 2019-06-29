8c7c9d3cf1d63eb777d760c7efe699515c0885db
vinayak <itssvinayak@gmail.com>
2019-06-29 17:48:35 +0530

    not working

--- file_converter/home/models.py
+++ file_converter/home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from json2xml import json2xml,readfromjson
 
 in_format = (
    ('xml', 'xml'),
@@ -18,3 +18,17 @@ class Document(models.Model):
     file_format=models.CharField(max_length=9, choices=in_format, default='1')
     format_output=models.CharField(max_length=9, choices=format_want, default='1')
     document = models.FileField(upload_to='documents/')
+
+    def save(self, **kwargs):
+        file = self.document.path
+        file_format_now=self.file_format
+        file_format_change_to=self.format_output
+
+        if file_format_now == 'json' and file_format_change_to == 'xml' :
+            print(file)
+            data =  readfromjson(file)
+            print(json2xml.Json2xml(data).to_xml())
+
+
+
+        super().save()
--- file_converter/home/views.py
+++ file_converter/home/views.py
@@ -1,37 +1,16 @@
 from django.shortcuts import render,redirect
 from .forms import DocumentForm
-from json2xml import json2xml,readfromjson
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-
 
 def index(request):
      if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
-
         if form.is_valid():
             form.save()
-            if request.POST.get('file_format') == 'json' and request.POST.get('format_output') == 'xml' :
-                print(jsontoxml(request.FILES['document'].name))
-
-
-        return redirect('index')
      else:
          form = DocumentForm()
      page_details={
      'title':'file converter: get freedom to convert file',
-     'form': form
+     'form': form,
+
      }
      return render(request,"home/index.html",page_details)
-
-def jsontoxml(filename):
-    data = readfromjson(MEDIA_ROOT+'/documents/'+filename)
-    data_xml=json2xml.Json2xml(data).to_xml()
-    url=MEDIA_ROOT+'/documents/'+os.path.splitext(filename)[0]+'.xml'
-    rurl=os.path.splitext(filename)[0]+'.xml'
-    with open(url, 'w+') as f:
-        f.write(data_xml)
-    os.remove(MEDIA_ROOT+'/documents/'+filename)
-    return rurl
