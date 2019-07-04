5837dc481657f4719fd768ef49238ba93a399073
itsvinayak <itssvinayak@gmail.com>
2019-07-04 10:52:47 +0530

    giving output

--- file_converter/home/models.py
+++ file_converter/home/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.conf import settings
+import os
+from django.core.files import File
 from json2xml import json2xml,readfromjson
+from django.core.files.base import ContentFile
 
 in_format = (
    ('xml', 'xml'),
@@ -17,18 +21,23 @@ format_want =  (
 class Document(models.Model):
     file_format=models.CharField(max_length=9, choices=in_format, default='1')
     format_output=models.CharField(max_length=9, choices=format_want, default='1')
-    document = models.FileField(upload_to='documents/')
+    document = models.FileField(upload_to="documents/")
 
     def save(self, **kwargs):
-        file = self.document.path
+        path=settings.MEDIA_ROOT
         file_format_now=self.file_format
         file_format_change_to=self.format_output
-
-        if file_format_now == 'json' and file_format_change_to == 'xml' :
-            print(file)
-            data =  readfromjson(file)
-            print(json2xml.Json2xml(data).to_xml())
-
-
-
         super().save()
+        file_name = self.document.name
+        print(file_name)
+        file = path+'/'+file_name
+        print(file)
+        name=file.split('.')[0]
+        print(name)
+        if file_format_now == 'json' and file_format_change_to == 'xml' :
+            data=readfromjson(file)
+            with open(file, 'w') as f:
+                 myfile = File(f)
+                 myfile.write(json2xml.Json2xml(data).to_xml())
+            myfile.closed
+            os.rename(file,name+'.xml')
--- file_converter/home/views.py
+++ file_converter/home/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render,redirect
 from .forms import DocumentForm
+from .models import Document
 
 def index(request):
      if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-     else:
-         form = DocumentForm()
+        name=request.FILES['document'].split('.')[0]+request.POST['format_output']
+        print(name)
+     form = DocumentForm()
      page_details={
      'title':'file converter: get freedom to convert file',
      'form': form,
-
      }
      return render(request,"home/index.html",page_details)
